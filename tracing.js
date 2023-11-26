86022716910c789d0df210f3c05db58c2d05781e
Alex Eum <aeum1003@gmail.com>
2023-11-26 15:03:29 -0500

    Final

--- tracing.js
+++ tracing.js
@@ -2,7 +2,6 @@ const { Resource } = require("@opentelemetry/resources");
 const {
   SemanticResourceAttributes,
 } = require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
@@ -15,9 +14,12 @@ const {
 } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+
 //Exporter
+
 module.exports = (serviceName) => {
-  const exporter = new ConsoleSpanExporter();
+  const exporter = new JaegerExporter();
   const provider = new NodeTracerProvider({
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
